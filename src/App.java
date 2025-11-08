90b63ff1d50ca680f17ee1cd2a9fae57634048f6
Nile Pereira <nilepereiraa@gmail.com>
2025-11-08 10:00:44 +0800

    FIrst edit test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Test lang natin di ko pa kasi gamay ang github lmao");
     }
 }
